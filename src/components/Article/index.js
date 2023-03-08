f840eac782fc2646d991949ccf87aec4e7b7addc
Clara <clarapachecodev@gmail.com>
2023-03-08 15:25:28 -0300

    Build and styled Post component and add 3 Post components inside Article component

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -3,9 +3,18 @@ import { Post } from '../Post'
 export function Article(){
  
   return (
-    <>
-      <h1>Article.js</h1>
-      <Post />
-    </>
+  
+      <section className="container">
+
+        <h1>Article.js</h1>
+        <div className="container-posts mt-5">
+        <Post />
+        <Post />
+        <Post />
+        </div>
+  
+
+      </section>
+    
   );
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,6 +1,26 @@
 export function Post(){
   
   return (
-    <h1>Posts</h1>
+    <div className="post mx-2">
+      <div className="img-post"></div>
+      <h5>Tecnologia</h5>
+      <h3>O guia definitivo do Blog</h3>
+      <p> Lorem ipsum dolor sit amet. Aut esse omnis qui quam
+        necessitatibus aut quaerat officiis eos sequi quasi?
+        Sit deserunt tempora et laborum praesentium est earum
+        fuga est libero Quis ut unde cumque sed earum odio.
+        Id autem rerum aut expedita quidem sit iure ipsum
+        est omnis nemo eum velit repudiandae. Aut quas quia
+        et aliquam laborum est nesciunt modi aut mollitia
+        aperiam est natus sapiente ut veniam fugiat in laborum enim.
+        </p>
+        <div className="flex pt-3">
+          <div className="img-profile"></div>
+          <div className="description-profile mt-2 ml-2">
+            <h6 className="color-blue">Fulano de Tal</h6>
+            <p>Aug2, 2020 - 8 min read</p>
+          </div>
+        </div>
+    </div>
   );
 }
