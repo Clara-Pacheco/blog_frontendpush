01fee7dad92407734c4bc73ad7148cbccefdf75d
Clara <clarapachecodev@gmail.com>
2023-03-08 10:56:47 -0300

    Build Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,28 @@
+import logo from '../../assets/svg/logo.svg'
+
 export function Header(){
+
+  const descLogo = 'Essa é a logo do Blog.'
 
   return (
 
-    <div>
-      <h1>
-        Header.js
-      </h1>
-   </div>
+   <>
+    <header className="flex-space-between">
+        <div className="logo">
+          <img src= {logo}   alt= {descLogo} />
+        </div>
+        <div className="search">
+          <input type="text" className="input-search" name="search" />
+        </div>
+        <ul className="menu">
+          <li><a href="#" className="nav-link">Categories</a></li>
+          <li><a href="#" className="nav-link">About</a></li>
+          <li><a href="#" className="nav-link">Contact</a></li>
+        </ul>
+      </header>
+    
+   
+   </>
 
   );
  
