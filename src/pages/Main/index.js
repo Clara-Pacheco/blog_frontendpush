860b55036c37664f02b5ea02a89072d829fa52b3
Clara <clarapachecodev@gmail.com>
2023-03-09 11:58:02 -0300

    Import api const and Post component to Main page component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,14 @@
+import { api } from '../../services/api'
+import { Post } from '../../components/Post'
+
 
 export function Main(){
   return (
+    <>
+    
     <h1>Main.js</h1>
+    
+    </>
 
 
 
